d8871ac54f1532317627ef79ddb8d45526a8f155
MrRainwater <jqrvz6@mst.edu>
2017-11-25 12:36:28 -0600

    create handler for new children

--- src/models/VideoStore.js
+++ src/models/VideoStore.js
@@ -1,10 +1,18 @@
 import { observable, computed } from 'mobx'
 import { database } from 'firebase'
+import YouTubeVideo from './Video'
+
+const videosRef = database().ref(`/videos`)
 
 class YouTubeVideoStore {
     @observable data = observable.map()
 
     constructor() {
+        videosRef.on('child_added', data => {
+            let video = new YouTubeVideo(data.val().id)
+            this.data.set(video.id, video)
+        })
+
         this.data = observable.map()
         this.fetchInitialData = this.fetchInitialData.bind(this)
     }
