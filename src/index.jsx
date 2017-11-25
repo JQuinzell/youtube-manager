29871c4cb3ed731b2279e24de30801f72a0ea1ec
MrRainwater <jqrvz6@mst.edu>
2017-11-25 00:05:29 -0600

    initialize test data

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,13 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import './firebase'
 
+import YouTubeVideoStore from './models/VideoStore'
+import YouTubeVideo from './models/Video'
+
+new YouTubeVideo('EgfOHQKldy0').save()
+new YouTubeVideo('qwOdU02SE0w').save()
+YouTubeVideoStore.fetchInitialData()
+
 ReactDOM.render(
     <h1>HELLO</h1>,
     document.getElementById('app')
