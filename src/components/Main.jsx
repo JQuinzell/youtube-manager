4b2c951a0a040a4a26782084bbd7e9b1f874cf6c
MrRainwater <jqrvz6@mst.edu>
2017-11-25 11:31:31 -0600

    main react component

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import CreateVideo from './CreateVideo'
+
+export default class Main extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <CreateVideo />
+    }
+}
