2514f2d812b2744887cbc3ae1cf404621ff42685
MrRainwater <jqrvz6@mst.edu>
2017-11-25 00:05:20 -0600

    video models

---
+++ src/models/Video.js
@@ -0,0 +1,19 @@
+import { database } from 'firebase'
+
+export default class YouTubeVideo {
+    id = null
+    tags = []
+
+    constructor(id) {
+        this.id = id
+
+        this.save = this.save.bind(this)
+    }
+
+    save() {
+        database().ref(`videos/${this.id}`).set({
+            id: this.id,
+            tags: this.tags
+        })
+    }
+}
---
+++ src/models/VideoStore.js
@@ -0,0 +1,26 @@
+import { observable, computed } from 'mobx'
+import { database } from 'firebase'
+
+class YouTubeVideoStore {
+    @observable data = observable.map()
+
+    constructor() {
+        this.fetchInitialData = this.fetchInitialData.bind(this)
+    }
+
+    @computed get videos() {
+        return this.data.values()
+    }
+
+    fetchInitialData() {
+        database().ref(`/videos`).once('value').then(snapshot => {
+            this.data = observable.map(snapshot.val())
+            console.log(this.videos.map(v => ({
+                id: v.id,
+                tags: v.tags
+            })))
+        })
+    }
+}
+
+export default new YouTubeVideoStore()
