b9b46afe404a62ee1107d0cef58d2ab5fd00909a
MrRainwater <jqrvz6@mst.edu>
2017-12-02 19:24:33 -0600

    add tag

---
+++ src/components/AddTag.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { Form, Text } from 'react-form'
+import { database } from 'firebase'
+
+export default class CreateVideo extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(data) {
+        this.props.video.addTag(data)
+    }
+
+    render() {
+        return <Form onSubmit={this.handleSubmit}>
+            {
+                formApi => (
+                    <form onSubmit={formApi.submitForm}>
+                        <label htmlFor="tagdata">Tag</label>
+                        <Text field="tagdata" id="tagdata" />
+                        <button type="submit">Submit</button>
+                    </form>
+                )
+            }
+        </Form>
+    }
+}
