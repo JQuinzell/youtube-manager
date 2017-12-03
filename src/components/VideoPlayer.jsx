f44d2a02319134e993dbba91d1632c6fb648f51b
FoxtrotFroey <sgp78@mst.edu>
2017-12-02 19:20:50 -0600

    Added tags

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -6,12 +6,21 @@ export default class VideoPlayer extends React.Component {
     }
 
     render() {
-        return <iframe
+        return <div>
+            <iframe
             className="videoitem" 
-            width="150" 
+            width="266" 
             height="150" 
             src={`https://www.youtube.com/embed/${this.props.video.id}`}
             frameborder="0" 
             allowfullscreen></iframe>
+
+            <div className="taggrid">
+                <div className="tagitem"> Tag 1</div>
+                <div className="tagitem"> Tag 2</div>
+                <div className="tagitem"> Tag 3</div>
+                <div className="tagitem"> Tag 4</div>
+            </div>    
+    </div>    
     }
 }
