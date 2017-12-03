02cdf182d3fd7803e7991c3eb25b77a2fc20fcf7
MrRainwater <jqrvz6@mst.edu>
2017-12-02 20:57:05 -0600

    remove  filler data

--- src/index.jsx
+++ src/index.jsx
@@ -7,9 +7,6 @@ import './main.css'
 import YouTubeVideoStore from './models/VideoStore'
 import YouTubeVideo from './models/Video'
 
-new YouTubeVideo('EgfOHQKldy0').save()
-new YouTubeVideo('qwOdU02SE0w').save()
-
 ReactDOM.render(
     <Main />,
     document.getElementById('app')
